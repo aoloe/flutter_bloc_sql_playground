a8d82559ec7ee6b32c1dba6b11abf14af8a269e2
ale rimoldi <ale@graphicslab.org>
2020-11-01 17:53:16 +0100

    initial commit with the code generated by android studio

---
+++ README.md
@@ -0,0 +1,4 @@
+# Flutter BLoC and Sql playground
+
+A tiny Flutter application for explring BLoC and Sql.
+
---
+++ android/app/src/main/java/ch/ideale/flutter_bloc_sql_playground/MainActivity.java
@@ -0,0 +1,6 @@
+package ch.ideale.flutter_bloc_sql_playground;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
